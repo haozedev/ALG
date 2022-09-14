c436fbfec36b512848017fa1014e643eb39fec29
haozedev <1823390765@qq.com>
2022-09-14 18:43:31 +0800

    正则

--- src/main/java/com/api/randomApi.java
+++ src/main/java/com/api/randomApi.java
@@ -19,7 +19,22 @@ public class randomApi {
 
         Logger logger = LoggerFactory.getLogger(randomApi.class);
 
-        logger.info("日志{},{}",num,i);
+        logger.info("日志{},问题{}",num,i);
 
+
+
+        String s = String.format("%-20s","0");
+
+        System.out.println(s);
+
+        String s1 = "(111)";
+        boolean contains = s1.contains("(");
+        System.out.println(contains);
+        String substring = s1.substring(s1.indexOf("(")+1, s1.indexOf(")"));
+        System.out.println(substring);
+
+        String s2 = "0123 20";
+        String s3 = s2.replaceFirst("0", "").replaceAll(" ","");
+        System.out.println(s3);
     }
 }
