7a7299f06cb36c5ad93029093434893100dbcc35
haozedev <1823390765@qq.com>
2023-03-05 22:11:59 +0800

    add 算法

--- src/main/java/com/pojo/MethodDO.java
+++ src/main/java/com/pojo/MethodDO.java
@@ -381,6 +381,31 @@ public class MethodDO {
         return res;
     }
 
+    public static int[] levelOrder(TreeNode root) {
+
+        if (root==null){
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>(){{ add(root); }};
+        ArrayList<Integer> ans = new ArrayList<>();
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            ans.add(node.val);
+            if (node.left!=null){
+                queue.add(node.left);
+            }
+            if (node.right!=null){
+                queue.add(node.right);
+            }
+        }
+
+        int []  res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+
     @Test
     public void methodTest() {
         String s = "abcdefg";
