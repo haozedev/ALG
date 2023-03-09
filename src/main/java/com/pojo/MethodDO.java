e24f63e797344ffafd73d0ea7b6ca3ffcb3ecf27
haozedev <1823390765@qq.com>
2023-03-09 22:00:24 +0800

    add 算法

--- src/main/java/com/pojo/MethodDO.java
+++ src/main/java/com/pojo/MethodDO.java
@@ -381,6 +381,11 @@ public class MethodDO {
         return res;
     }
 
+    /**
+     * 从上到下遍历二叉树
+     * @param root
+     * @return
+     */
     public static int[] levelOrder(TreeNode root) {
 
         if (root==null){
@@ -404,6 +409,37 @@ public class MethodDO {
             res[i] = ans.get(i);
         }
         return res;
+    }
+
+    /**
+     * 剑指 Offer 32 - II. 从上到下打印二叉树 II,从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root==null){
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+
+        return ret;
     }
 
     /**
