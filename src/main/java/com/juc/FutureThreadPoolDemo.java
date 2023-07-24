f083873ba90d6470cde828f1be55bba28bf78748
haozedev <1823390765@qq.com>
2023-07-24 23:17:32 +0800

    add future

---
+++ src/main/java/com/juc/FutureThreadPoolDemo.java
@@ -0,0 +1,16 @@
+package com.juc;
+
+/**
+ * @ClassName FutureThreadPoolDemo
+ * @Author haoZe
+ * @Date 2023/4/24
+ **/
+public class FutureThreadPoolDemo {
+    public static void main(String[] args) {
+
+    }
+    private static void m1(){
+        
+    }
+
+}
