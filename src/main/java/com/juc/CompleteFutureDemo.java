4994c1f012f8afd8ec8d1140ee078e184968f3bf
haozedev <1823390765@qq.com>
2023-10-12 11:04:41 +0800

    add future test

---
+++ src/main/java/com/juc/CompleteFutureDemo.java
@@ -0,0 +1,36 @@
+package com.juc;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author haoze
+ * @create 2023/10/12 10:37
+ */
+public class CompleteFutureDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        FutureTask<String> futureTask = new FutureTask<>(new MyThread2());
+
+        Thread t1 = new Thread(futureTask, "t1");
+        t1.start();
+        System.out.println(futureTask.get());
+    }
+}
+
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class MyThread2 implements Callable<String>{
+    @Override
+    public String call() throws Exception {
+        System.out.println("------come in call() ");
+        return "hello callable";
+    }
+}
