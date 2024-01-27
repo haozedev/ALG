07788b926a8855eeece6226edbb310f287a9d831
haozedev <1823390765@qq.com>
2024-01-27 13:50:59 +0800

    add 2859. 计算 K 置位下标对应元素的和

--- src/main/java/com/pojo/MethodDO.java
+++ src/main/java/com/pojo/MethodDO.java
@@ -800,27 +800,6 @@ public class MethodDO {
         return sum;
     }
 
-    public long maximumSumOfHeights(List<Integer> maxHeights) {
-        int n = maxHeights.size();
-        long res = 0;
-        for (int i = 0; i < n; i++) {
-            long pre = maxHeights.get(i);
-            long sum = pre;
-            for (int j = i - 1; j >= 0; j--) {
-                pre = Math.min(pre, maxHeights.get(j));
-                sum += pre;
-            }
-
-            long suf = maxHeights.get(i);
-            for (int j = i + 1; j < n; j++) {
-                suf = Math.min(suf, maxHeights.get(j));
-                sum += suf;
-            }
-
-            res = Math.max(res, sum);
-        }
-        return res;
-    }
 
     public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
         int ans = 0;
